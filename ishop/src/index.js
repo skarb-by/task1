6fca7955dbd5cdc2ad909de9d3f1257dd877df74
skarb-by <skarb_gaspadar@mail.ru>
2023-10-20 09:00:31 +0300

    20102023

--- ishop/src/index.js
+++ ishop/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import ReactDOMClient from 'react-dom/client';
+import * as ReactDOMClient from 'react-dom/client';
 import './index.css';
 import Shop from './components/Shop';
 
