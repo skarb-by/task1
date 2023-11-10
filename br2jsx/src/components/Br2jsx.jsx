5951beabfc5fcc5d522268ce7763948d08566188
skarb-by <skarb_gaspadar@mail.ru>
2023-11-10 17:04:41 +0300

    10112023

--- br2jsx/src/components/Br2jsx.jsx
+++ br2jsx/src/components/Br2jsx.jsx
@@ -1,26 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./Br2jsx.css";
-import To from "./To";
-
 class Br2jsx extends React.Component {
   static propTypes = {
     text: PropTypes.string,
   };
 
   render() {
-    const textArray = this.props.text.split(/<br\s?\/?>/);
-
-    const text = textArray.map((el, id) => {
+    const text = this.props.text.split(/<br\s?\/?>/).map((el, id) => {
       return (
-        <To key={id}>
+        <>
           {id !== 0 && <br />} {el}
-        </To>
+        </>
       );
     });
 
     return <div className="Br2jsx">{text}</div>;
   }
 }
-
 export default Br2jsx;
+
--- br2jsx/src/components/To.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-class To extends React.Component {
-  render() {
-    return <>{this.props.children}</>;
-  }
-}
-
-export default To;
